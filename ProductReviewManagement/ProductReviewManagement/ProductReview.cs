7e58edf61df3decd3f0793322a7f76a20fe2edac
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-03-05 15:55:26 +0530

    [shweta] Created Table And Added Data to the table

---
+++ ProductReviewManagement/ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    //Creating class for declaring product review properties 
+    public class ProductReview
+    {
+        //Declaring properties
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+
+        //Method to override tostring for displaying assigned values
+        public override string ToString()
+        {
+            return $"Product Id : {ProductId}  \tUser Id : {UserId}  \tProduct Rating : {Rating}  \tProduct Review : {Review}    \tIsLike : {IsLike}";
+        }
+    }
+}
--- ProductReviewManagement/ProductReviewManagement/Program.cs
+++ ProductReviewManagement/ProductReviewManagement/Program.cs
@@ -6,13 +6,49 @@ using System.Threading.Tasks;
 
 namespace ProductReviewManagement
 {
-    internal class Program
+    public class Program
     {
+        //Declaring variable
+        public static List<ProductReview> listOfProduct;
         static void Main(string[] args)
         {
             Console.WriteLine("WEL-COME to product review management program");
-
+            //Creating object of product review
+            //List<ProductReview> products = new List<ProductReview>();
+            try
+            {
+               Console.WriteLine("1: Add Product Review To List \n2: Show All Product Review \n3: Exit");
+               Console.Write("Choose the option from above : ");
+               int option=Convert.ToInt32(Console.ReadLine());
+               bool flag = true;
+               switch (option)
+               {
+                 case 1:
+                       //Calling the method of adding product review to list(UC1)
+                       //products=ProductReviewManager.AddProductReviewToList();
+                       listOfProduct = ProductReviewManager.AddProductReviewToList();
+                        //Console.WriteLine(listOfProduct);
+                       break;
+                  case 2:
+                        //Calling the method to show the product review list(UC1)
+                        listOfProduct = ProductReviewManager.AddProductReviewToList();
+                        ProductReviewManager.IterateOverList(listOfProduct);
+                          break;
+                   case 3:
+                         flag = false;
+                          break;
+                   default:
+                                Console.WriteLine("Wrong choice choose again");
+                        break;
+               }                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
         }
+
+
     }
 }
